a3cf47bf8d9152807888d68105e1226fa7d34c70
Ibrahim Al-Zuaby <izuaby@gmail.com>
2025-11-18 18:12:00 +0200

    Logo Added

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,4 +1,6 @@
 import { ThemeToggle } from "@/components/theme-toggle";
+import Image from "next/image";
+
 
 const navLinks = [
     {href: "/about", label: "About"},
@@ -23,18 +25,25 @@ export function Header() {
         </nav>
         <div className="mx-4 flex transition-all duration-300 ease-in-out group-hover:mx-6">
           <a href="/" className="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="h-8 w-8 text-primary"
-            >
-              <path d="M12.22 2h-4.44l-3 9h5l-3.33 11h8.89l3-9h-5l3.33-11z" />
-            </svg>
+            {/*<svg*/}
+            {/*  xmlns="http://www.w3.org/2000/svg"*/}
+            {/*  viewBox="0 0 24 24"*/}
+            {/*  fill="none"*/}
+            {/*  stroke="currentColor"*/}
+            {/*  strokeWidth="2"*/}
+            {/*  strokeLinecap="round"*/}
+            {/*  strokeLinejoin="round"*/}
+            {/*  className="h-8 w-8 text-primary"*/}
+            {/*>*/}
+            {/*  <path d="M12.22 2h-4.44l-3 9h5l-3.33 11h8.89l3-9h-5l3.33-11z" />*/}
+            {/*</svg>*/}
+              <Image
+                src="/assets/Logo.png"
+                alt="Icon"
+                width={32}
+                height={32}
+                className="h-8 w-8"
+              />
             <span className="font-bold hidden md:inline-block transition-all duration-300 ease-in-out">NexoForger</span>
           </a>
         </div>
